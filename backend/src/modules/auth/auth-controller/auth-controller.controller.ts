cba4fc9fda66c5ea066aed5aa98ff2ecec41ab8a
Gabriela <gabrielasan07@gmail.com>
2026-05-12 23:34:26 -0300

    auth controllers y services.
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- backend/src/modules/auth/auth-controller/auth-controller.controller.ts
+++ backend/src/modules/auth/auth-controller/auth-controller.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Body,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { LoginDto } from '../dtos/input/login.dtos';
+import { AuthServiceService } from '../auth-service/auth-service.service';
 
-@Controller('auth-controller')
-export class AuthControllerController {}
+@Controller('auth')
+export class AuthControllerController {
+  constructor(private readonly authService: AuthServiceService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async login(@Body() dto: LoginDto): Promise<{ accessToken: string }> {
+    return await this.authService.login(dto);
+  }
+}
--- backend/src/modules/auth/auth-service/auth-service.service.ts
+++ backend/src/modules/auth/auth-service/auth-service.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from '../dtos/input/login.dtos';
+import { UsuarioServiceService } from '../usuarios-service/usuarios-service.service';
 
 @Injectable()
-export class AuthServiceService {}
+export class AuthServiceService {
+  
+  constructor(
+    private readonly usuariosService: UsuarioServiceService, 
+    private readonly jwtService: JwtService
+  ) {}
+
+  async login(dto: LoginDto): Promise<{ accessToken: string }> {
+    const usuario = await this.usuariosService.findByNombre(dto.nombre); //busca por el nombre
+
+    if (!usuario) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    const isClaveValida = await bcrypt.compare(dto.clave, usuario.clave);
+
+    if (!isClaveValida) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    const payload = { 
+      sub: usuario.id, 
+      nombre: usuario.nombre 
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload) 
+    };
+ } 
+}
+}
