9cca20988c0b7986db8af8ec484fbd2a0ee2a029
Gabriela <gabrielasan07@gmail.com>
2026-05-14 00:50:08 -0300

    correcion en auth guard Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- backend/src/modules/auth/auth-guard/auth-guard.guard.ts
+++ backend/src/modules/auth/auth-guard/auth-guard.guard.ts
@@ -1,10 +1,19 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
+interface JwtPayload {
+  sub: number;
+  nombre: string;
+}
 @Injectable()
 export class AuthGuardGuard implements CanActivate {
-  constructor(private jwtService: JwtService) { }
+  constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
@@ -14,16 +23,16 @@ export class AuthGuardGuard implements CanActivate {
       throw new UnauthorizedException('Token no autorizado');
     }
     try {
-      const payload = await this.jwtService.verifyAsync(accessToken);
+      const payload: JwtPayload =
+        await this.jwtService.verifyAsync(accessToken);
       request['usuario'] = payload;
-
     } catch {
       throw new UnauthorizedException('Token expirado');
     }
     return true;
   }
-   private extractTokenFromHeader(request: Request): string | undefined {
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
-},
-
+  }
 }
