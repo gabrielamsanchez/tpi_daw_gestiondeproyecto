a0f5a5c6c25ed16b44316b60e737cf3b3f87b051
Victoria Ines <inessavalos14@gmail.com>
2026-05-17 19:49:58 -0300

    fic: correccion del app module. Login funcionando correctamente en swagger y bruno

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,39 +1,30 @@
 import { Module } from '@nestjs/common';
-import { GestionModule } from './modules/gestion/gestion.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TareasModule } from './modules/gestion/tareas/tareas.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+
+import { DatabaseModule } from './modules/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsuariosModule } from './modules/auth/usuarios/usuarios.module';
-import { DatabaseModule } from './modules/database/database.module';
+import { GestionModule } from './modules/gestion/gestion.module';
+import { TareasModule } from './modules/gestion/tareas/tareas.module';
+//import { ClientesModule } from './modules/gestion/clientes/clientes.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'src/.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        // synchronize: false, pa que ande
-      }),
-      inject: [ConfigService],
-    }),
+   
     DatabaseModule,
     AuthModule,
     UsuariosModule,
+    //ClientesModule,
     TareasModule,
     GestionModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
