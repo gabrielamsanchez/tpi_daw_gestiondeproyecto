2f6a047369996465f9e7e109e906fb970d9c6b92
Gabriela <gabrielasan07@gmail.com>
2026-05-17 02:31:54 -0300

    correción de auth login y modulos
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,8 +22,10 @@ import { DatabaseModule } from './modules/database/database.module';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
         synchronize: true,
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        // synchronize: false, pa que ande
       }),
       inject: [ConfigService],
     }),
