501aff2c7f4d35ac02e683d31ff45da028e332b2
noahcassutti <noahncassutti@gmail.com>
2026-05-27 01:43:09 -0300

    changes fix cliente and routes layout
    Co-authored-by: Santiago <SantiagoBertorello@users.noreply.github.com>

--- frontend/src/app/features/clientes/pages/cliente/cliente.ts
+++ frontend/src/app/features/clientes/pages/cliente/cliente.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
 
+import { Sidebar } from '../../../../shared/components/sidebar/sidebar';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
+import { CommonModule } from '@angular/common';
+import { MessageService, SelectItem } from 'primeng/api';
+import { Card } from 'primeng/card';
+
+interface ClienteInterface {
+    id: string;
+    nombre: string;
+    estado: string;
+}
 
 @Component({
-  selector: 'app-cliente',
-  imports: [],
-  templateUrl: './cliente.html',
-  styleUrl: './cliente.css',
+    selector: 'app-tabla-clientes',
+    standalone: true,
+    imports: [
+        CommonModule,
+        Card, // 2. IMPORTANTE: Agregamos Card acá para que el HTML lo reconozca
+        SelectModule,
+        TableModule,
+        TagModule,
+        ToastModule,
+        ButtonModule,
+        InputTextModule,
+        RippleModule,
+        FormsModule,
+    ],
+    providers: [MessageService],
+    templateUrl: './cliente.html'
 })
-export class Cliente {}
+export class Cliente implements OnInit {
+    private messageService = inject(MessageService);
+
+    clientes: ClienteInterface[] = [];
+    estados: SelectItem[] = [];
+    clonedClientes: { [s: string]: ClienteInterface } = {};
+
+    ngOnInit() {
+        this.clientes = [
+            { id: '1', nombre: '', estado: '' },
+            { id: '2', nombre: '', estado: '' },
+            { id: '3', nombre: '', estado: '' },
+            { id: '4', nombre: '', estado: '' }
+        ];
+
+        this.estados = [
+            { label: 'Activo', value: 'Activo' },
+            { label: 'Inactivo', value: 'Inactivo' }
+        ];
+    }
+
+    onRowEditInit(cliente: ClienteInterface) {
+        this.clonedClientes[cliente.id] = { ...cliente };
+    }
+
+    onRowEditSave(cliente: ClienteInterface) {
+        delete this.clonedClientes[cliente.id];
+        this.messageService.add({ severity: 'success', summary: 'Éxito', detail: 'Cliente actualizado' });
+    }
+
+    onRowEditCancel(cliente: ClienteInterface, index: number) {
+        this.clientes[index] = this.clonedClientes[cliente.id];
+        delete this.clonedClientes[cliente.id];
+    }
+}
