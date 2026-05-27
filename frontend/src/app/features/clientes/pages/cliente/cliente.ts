a46d03a34f554fc0578ae6eddd085af78b3042ee
noahcassutti <noahncassutti@gmail.com>
2026-05-26 23:28:08 -0300

    creation features cliente

---
+++ frontend/src/app/features/clientes/pages/cliente/cliente.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cliente',
+  imports: [],
+  templateUrl: './cliente.html',
+  styleUrl: './cliente.css',
+})
+export class Cliente {}
