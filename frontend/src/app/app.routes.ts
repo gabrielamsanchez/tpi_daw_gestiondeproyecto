9feb4d20cfdf6d13b0e5446c1b1ec94140f5df3a
Gabriela <gabrielasan07@gmail.com>
2026-05-27 01:06:44 -0300

    add usuarios vistas y estilos
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/pages/login/login';
 import { Home } from './features/dashboard/pages/home/home';
+import { Usuarios } from './features/usuarios/usuarios';
+
 export const routes: Routes = [
-     {
+    {
         path: "login",
         component: Login
     },
     { path: "dashboard",
       component: Home
+    },
+    { path: "usuarios",
+      component: Usuarios
     }
 ];
--- frontend/src/app/features/usuarios/usuarios.ts
+++ frontend/src/app/features/usuarios/usuarios.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+//import { Sidebar } from '../../shared/components/sidebar/sidebar';
+
+interface Usuario {
+  nombre: string;
+  estado: string;
+}
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TableModule, ButtonModule, ],
   templateUrl: './usuarios.html',
   styleUrl: './usuarios.css',
 })
-export class Usuarios {}
+export class Usuarios implements OnInit {
+  loading = false;
+  usuarios: Usuario[] = [];
+
+  ngOnInit() {
+    this.usuarios = [//despues cambiar, es solo prueba
+      { nombre: 'Ana', estado: 'ACTIVO' },
+      { nombre: 'Luis', estado: 'ACTIVO' },
+      { nombre: 'María', estado: 'INACTIVO' }
+    ];
+  }
+}
