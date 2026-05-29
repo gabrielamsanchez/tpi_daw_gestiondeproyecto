aad1a855e2b270433b1d26a030101aea0abd0091
Santiago Bertorello <santinbertorello@gmail.com>
2026-05-29 06:22:39 -0300

    add generacion de modulo estadisticas(a completar)

--- backend/src/modules/gestion/gestion.module.ts
+++ backend/src/modules/gestion/gestion.module.ts
@@ -3,8 +3,9 @@ import { ClientesModule } from './clientes/clientes.module';
 import { ProyectosModule } from './proyectos/proyectos.module';
 import { TareasModule } from './tareas/tareas.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { EstadisticasModule } from './estadisticas/estadisticas.module';
 
 @Module({
-  imports: [UsuariosModule, ClientesModule, ProyectosModule, TareasModule],
+  imports: [UsuariosModule, ClientesModule, ProyectosModule, TareasModule, EstadisticasModule],
 })
 export class GestionModule {}
