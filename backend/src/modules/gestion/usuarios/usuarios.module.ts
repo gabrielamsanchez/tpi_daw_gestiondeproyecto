ea9542b4c67ce67113bd3d747381797e599e8aba
Gabriela <gabrielasan07@gmail.com>
2026-05-31 03:05:08 -0300

    fix codigo de usuario
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>

--- backend/src/modules/gestion/usuarios/usuarios.module.ts
+++ backend/src/modules/gestion/usuarios/usuarios.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioServiceService } from './usuario-service/usuario-service.service';
 import { Usuario } from './entities/input/usuario-entities';
+import { UsuariosController } from './controllers/usuarios.controllers';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
+  controllers: [UsuariosController],
   providers: [UsuarioServiceService],
   exports: [UsuarioServiceService, TypeOrmModule],
 })
