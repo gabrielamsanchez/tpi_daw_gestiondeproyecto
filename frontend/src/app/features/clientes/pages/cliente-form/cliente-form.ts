82ca1ced3d016d4a425c3481f2c3c5e2f84b21af
Victoria Ines <inessavalos14@gmail.com>
2026-06-10 21:52:32 -0300

    FIX: corrección en clintes

--- frontend/src/app/features/clientes/pages/cliente-form/cliente-form.ts
+++ frontend/src/app/features/clientes/pages/cliente-form/cliente-form.ts
@@ -1,47 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { InputTextModule } from 'primeng/inputtext';
-// import { ButtonModule } from 'primeng/button';
-// import { SelectModule } from 'primeng/select';
-// import { DynamicDialogRef } from 'primeng/dynamicdialog';
-
-// @Component({
-//   selector: 'app-cliente-form',
-//   imports: [CommonModule, FormsModule, InputTextModule, ButtonModule, SelectModule],
-//   templateUrl: './cliente-form.html',
-//   styleUrl: './cliente-form.css',
-// })
-
-// export class ClienteForm implements OnInit {
-
-  
-//   cliente = {
-//     nombre: '',
-//     estado: 'Activo', 
-//     telefono: '',
-//     correo: ''
-//   };
-
-//   estadosDisponibles: string[] = [];
-
-//   constructor(public ref: DynamicDialogRef) {}
-
-//   ngOnInit() {
-//     this.estadosDisponibles = ['Activo', 'Inactivo'];
-//   }
-
-//   guardar() {
-//     // Validación simple: obligamos a que ponga mínimo el nombre
-//     if (this.cliente.nombre.trim()) {
-//       this.ref.close(this.cliente); 
-//     }
-//   }
-
-//   cerrar() {
-//     this.ref.close(); 
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -86,7 +42,6 @@ export class ClienteForm implements OnInit {
   }
 
   formularioValido(): boolean {
-    // Valida que nada esté vacío y se cumplan los formatos
     return this.cliente.nombre.trim().length >= 5 && 
            this.cliente.estado !== null &&
            this.cliente.estado !== '' &&
